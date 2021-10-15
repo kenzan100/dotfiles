d689114d62241bd05d74807c5107be93ddb4e586
Yuta Miyama <s04155yo@gmail.com>
2021-10-15 15:12:09 -0400

    Remove a line which was not compatible with my cloud environment

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ echo """[alias]
   st = status
   ci = commit
   brb = branch --sort=committerdate
-  diffr = diff --color-words --word-diff-regex='\\w+|[^[:space:]]'
   diffc = diff --cached
   pushf = push --force-with-lease
 """ >> ~/.gitconfig
